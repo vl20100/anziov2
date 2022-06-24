1342cba139bb8a6958143e505b65cf81ad2fc920
Vincent Leclercq <vleclercq@jeanlain.com>
2022-06-24 23:11:57 +0200

    Added top bar

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('reset', './assets/scripts/common/reset.js')
+    .addEntry('common_topbar', './assets/scripts/common/topbar.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -56,7 +56,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
