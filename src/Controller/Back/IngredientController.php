8fff5565811a30dd30783d9d286c000e13772629
Vincent Leclercq <vleclercq@jeanlain.com>
2022-06-24 21:35:41 +0200

    CRUD Ingredient

---
+++ src/Controller/Back/IngredientController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/administranzio/ingredient')]
+class IngredientController extends AbstractController
+{
+    #[Route('/', methods: ['GET'])]
+    public function index(IngredientRepository $ingredientRepository): Response
+    {
+        return $this->render('back/ingredient/index.html.twig', [
+            'ingredients' => $ingredientRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', methods: ['GET', 'POST'])]
+    public function new(Request $request, IngredientRepository $ingredientRepository): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredientRepository->add($ingredient, true);
+
+            return $this->redirectToRoute('app_back_ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/ingredient/new.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(Ingredient $ingredient): Response
+    {
+        return $this->render('back/ingredient/show.html.twig', [
+            'ingredient' => $ingredient,
+        ]);
+    }
+
+    #[Route('/{id}/edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ingredient $ingredient, IngredientRepository $ingredientRepository): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredientRepository->add($ingredient, true);
+
+            return $this->redirectToRoute('app_back_ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', methods: ['POST'])]
+    public function delete(Request $request, Ingredient $ingredient, IngredientRepository $ingredientRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ingredient->getId(), $request->request->get('_token'))) {
+            $ingredientRepository->remove($ingredient, true);
+        }
+
+        return $this->redirectToRoute('app_back_ingredient_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
