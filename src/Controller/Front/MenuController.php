3e786ca6f74a77360709c1a88ebb4183703ad7ad
Vincent Leclercq <vleclercq@jeanlain.com>
2022-08-06 13:59:24 +0200

    Update Menu

--- src/Controller/Front/MenuController.php
+++ src/Controller/Front/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\PizzaBaseRepository;
 use App\Repository\PizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,12 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu')]
-    public function menu(Request $request, PizzaRepository $pizzaRepository): Response
+    public function menu(Request $request, PizzaRepository $pizzaRepository, PizzaBaseRepository $baseRepository): Response
     {
         $pizzas = $pizzaRepository->findAll();
+        $bases = $baseRepository->findAll();
 
         return $this->render('front/menu.html.twig', [
-            'pizzas' => $pizzas
+            'pizzas' => $pizzas,
+            'bases' => $bases
         ]);
     }
 }
